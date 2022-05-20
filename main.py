f7e5a6c77e92cf9161b9b7c829292b1d6595c34f
RidusM <79109438+RidusM@users.noreply.github.com>
2022-05-20 10:00:25 +0300

    Initial Commit v1.1

--- main.py
+++ main.py
@@ -3,19 +3,36 @@ from telebot import types
 
 token = '5367696164:AAHX8QGlpmlTMcvzgcx5QmYnv9KDs1X231I'
 bot = telebot.TeleBot(token)
-
+conn = sl.connect('Stabis.db', check_same_thread=False)
+cursor = conn.cursor()
+itr = cursor.execute("SELECT Location FROM Users").fetchall()
+select_Info = []
+for abInfo in itr:
+    select_Info.append(abInfo[])
+print(abInfo)
 tconv = lambda x: time.strftime("%H:%M:%S", time.localtime(x))
 tcoj = lambda x: time.strftime("%d.%m.%Y", time.localtime(x))
 
-conn = sl.connect('Stabis.db', check_same_thread=False)
-cursor = conn.cursor()
 
-def db_table_val(ID_User: int, Date: str, Time:str, Location:str, Latitude:str, Longitude:str):
-    cursor.execute('INSERT INTO Users (ID_User, Date, Time, Location, Latitude, Longitude) VALUES (?,?,?,?,?,?)',(ID_User, Date, Time, Location, Latitude, Longitude))
+
+def db_table_val(ID_User: int, Date: str, Time:str, Location:str):
+    conn = sl.connect('Stabis.db', check_same_thread=False)
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO Users (ID_User, Date, Time, Location) VALUES (?,?,?,?)',(ID_User, Date, Time, Location))
     conn.commit()
-def db_table_update(Latitude: str, Longitude: str):
-    cursor.execute('UPDATE Users SET Latitude = ?, Longitude = ?')
-    conn.commit
+    cursor.close()
+    conn.close()
+def db_table_update(Latitude: str, Longitude: str, Location: str):
+    conn = sl.connect('Stabis.db', check_same_thread=False)
+    cursor = conn.cursor()
+    cursor.execute("UPDATE Users SET Latitude = ?, Longitude = ? WHERE Location = 'Bruh'", (Latitude, Longitude, Location))
+def db_table_delete(Location:str):
+    conn = sl.connect('Stabis.db', check_same_thread=False)
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM Users WHERE Location = (?)", (Location, ))
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -30,7 +47,7 @@ def start(message):
 def button_message(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1 = types.KeyboardButton('Добавить название объекта')
-    item2 = types.KeyboardButton('Отправить геолокацию', request_location=True)
+    item2 = types.KeyboardButton('Отправить геолокацию')
     item3 = types.KeyboardButton('Реестр объектов')
     item4 = types.KeyboardButton('Удалить объект')
     markup.row(item1)
@@ -45,16 +62,28 @@ def add_object(message):
 
 def add_object_2(message):
     bot.send_message(message.chat.id, 'Мы запомнили это')
-    print(message.text)
+    us_id = message.from_user.id
+    us_date = tconv(message.date)
+    us_time = tcoj(message.date)
+    db_table_val(ID_User=us_id, Date= us_date, Time= us_time, Location=message.text)
 
-'''@bot.message_handler(func=lambda message: message.text == "Отправить геолокацию")
-def add_geoloc(message):
-    msg = bot.send_message(message.chat.id, "Отправьте геолокацию")
-    bot.register_next_step_handler(msg, add_geoloc2)
+@bot.message_handler(func=lambda message: message.text == "Отправить геолокацию")
+def add_geolog(message):
+    msg = bot.send_message(message.chat.id, "Выберите объект", reply_markup=makeKeyboard())
+    bot.register_next_step_handler(msg, add_geolog2)
 
-def add_geoloc2(message):
-    bot.send_message(message.chat.id, "Мы запомнили и это")
-    print(message.text) '''
+def add_geolog2(message):
+    bot.send_message(message.chat.id, "Геолокация добавлена")
+    db_table_update(Latitude=us_latitude, Longitude=us_longitude, Location=us_location)
+
+@bot.message_handler(func=lambda message: message.text == "Удалить объект")
+def del_object(message):
+    msg = bot.send_message(message.chat.id, "Введите название объекта")
+    bot.register_next_step_handler(msg, del_object2)
+
+def del_object2(message):
+    bot.send_message(message.chat.id, "Удаление прошло успешно")
+    db_table_delete(Location=message.text)
 
 @bot.message_handler(content_types=["location"])
 def location(message):
@@ -62,13 +91,9 @@ def location(message):
         bot.send_message == (message.location)
         coords = geocoder(message.location.latitude, message.location.longitude)
         bot.send_message(message.chat.id, coords)
-        us_id = message.from_user.id
-        us_date = tconv(message.date)
-        us_time = tcoj(message.date)
         us_latitude = message.location.latitude
         us_longitude = message.location.longitude
-        us_location = coords
-        db_table_val(ID_User=us_id, Date=us_date, Time=us_time, Location=us_location, Latitude=us_latitude, Longitude=us_longitude)
+        db_table_update(Latitude=us_latitude, Longitude=us_longitude)
 
 
 def geocoder(latitude, longitude):
@@ -79,5 +104,11 @@ def geocoder(latitude, longitude):
         return f'{address["address"].get("city")}, {address["address"].get("road")}, {address["address"].get("house_number")} '
     except Exception as e:
         return 'error'
+
+'''def makeKeyboard():
+    markup5 = types.InlineKeyboardMarkup()
+    for row in obInfo:
+        markup5.add(types.InlineKeyboardButton(text=result, callback_data=select_Info"['result', '" + str(result) + "']"))
+    return markup5'''
 
 bot.polling(none_stop=True)
